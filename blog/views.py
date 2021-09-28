13d63828c90edf3655c004df0dee1ff757ebf9af
Chris Daming <chris@legalgps.com>
2021-09-27 22:09:16 -0500

    FSDI 112 assignment 3 and comp report

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,43 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (CreateView, UpdateView, DeleteView)
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+from django.urls import reverse_lazy
+from django.forms import UserCreationForm
 from .models import Post
 
 
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    field = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
