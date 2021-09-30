d86ceae8b7b932a9d1e50975320c701d03283bdc
Chris Daming <chris@legalgps.com>
2021-09-29 21:41:19 -0500

    updated allwoed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e7=#yqw-f7yln8$f!@zxd8utbfzgcr3sso$yd-bdh*vd_74((y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
