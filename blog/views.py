b0b7d30cade9d8f916d2acf550f2f483cf9b693d
Chris Daming <chris@legalgps.com>
2021-10-02 11:22:57 -0500

    class 2 blog update

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (CreateView, UpdateView, DeleteView)
 
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,UserPassesTestMixin,)
 from django.urls import reverse_lazy
 from .models import Post
 
@@ -23,13 +23,20 @@ class BlogCreateView(LoginRequiredMixin, CreateView):
     fields = ["title", "author", "body"]
 
 
-class BlogUpdateView(LoginRequiredMixin, UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ["title", "body"]
 
+    def test_func(self):
+        obj = self.get_object()   
+        return obj.author == self.request.user
 
-class BlogDeleteView(LoginRequiredMixin, DeleteView):
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "post_delete.html"
     success_url = reverse_lazy("home")
+
+    def test_func(self):
+        obj = self.get_object()   
+        return obj.author == self.request.user
